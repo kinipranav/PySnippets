0ff0079de7c45c7c2a85e6454ed39dc2cfcb5d2b
Pranav <pranav.kini@fmr.com>
2018-07-03 15:40:25 +0530

    util script with useful oneliners & short snippets

---
+++ utilScript.py
@@ -0,0 +1,7 @@
+#one liner to export source code of any function from a library
+python -c 'import inspect,os; print(inspect.getsource(os))'
+
+#same can be done with iPython
+ipython
+import os
+os.path.splitt??
